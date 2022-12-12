81c2d52587756da833bd940472c553af4cddc802
Hakhyun Kim <kimhakhyun@gmail.com>
2022-12-13 01:30:42 +0900

    Change compilation

---
+++ Source/SolTestCPP/SolTestCPP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SolTestCPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SolTestCPP, "SolTestCPP" );
---
+++ Source/SolTestCPP/SolTestCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SolTestCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOLTESTCPP_API ASolTestCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
