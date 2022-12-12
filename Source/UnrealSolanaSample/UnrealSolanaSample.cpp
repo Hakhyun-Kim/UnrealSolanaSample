6e06dfb111a825fd51f41f64ed4e09e8fddd3504
Hakhyun Kim <kimhakhyun@gmail.com>
2022-12-12 17:06:53 +0900

    Unreal Base Sample Init

---
+++ Source/UnrealSolanaSample/UnrealSolanaSample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealSolanaSample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealSolanaSample, "UnrealSolanaSample" );
